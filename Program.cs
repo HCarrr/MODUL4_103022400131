3beae73a8b2d1b078cb859cc3e1e728dcbea1260
HCarrr <huda012354@gmail.com>
2026-03-17 07:02:50 +0700

    second commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class KodePaket
+{
+    String[] namaPaket = { "Basic", "Standard", "Premium", "Unlimited", "Gaming", "Streaming" , "Family", "Business", "Student", "Traveler" };
+    String[] kodePaket = { "P201", "P202", "P203", "P204", "P205", "P206", "P207", "P208", "P209", "P210" };
+
+    public int getNamaPaket(String nama)
+    {
+        for (int i = 0; i < kodePaket.Length; i++)
+        {
+            if (namaPaket[i] == nama)
+            {
+                return i;
+            }
+        }
+        return -1; 
+    }
+
+    static void Main(string[] args)
+    {
+        KodePaket paket = new KodePaket();
+        Console.Write("Masukkan nama paket: ");
+        String inputNama = Console.ReadLine();
+        int index = paket.getNamaPaket(inputNama);
+        if (index != -1)
+        {
+            Console.WriteLine("Kode paket untuk " + inputNama + " adalah: " + paket.kodePaket[index]);
+        }
+        else
+        {
+            Console.WriteLine("Nama paket tidak ditemukan.");
+        }
+    }
+};
--- obj/Debug/net10.0/MODUL4_103022400131.AssemblyInfo.cs
+++ obj/Debug/net10.0/MODUL4_103022400131.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MODUL4_103022400131")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a10577b89216c4c5f1b2cf217cf4de0995ad984")]
 [assembly: System.Reflection.AssemblyProductAttribute("MODUL4_103022400131")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MODUL4_103022400131")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
